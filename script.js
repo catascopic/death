6d1031f6c65a8df562fcc94833f77b60dd740489
Max Koren <Koren_Max@bah.com>
2020-05-19 17:53:44 -0400

    marbles

--- script.js
+++ script.js
@@ -1,74 +1,86 @@
+const RADIUS = 8;
+const DIAMETER = RADIUS * 2;
+const SIN_60 = 0.86602540378;
+
+const FRAME_INTERVAL = 20;
+const BASE_RATE = 2000;
+const SPEED = 10;
+
+var intervalId;
 var canvas;
 var context;
 
-var intervalId;
-
-// change this to modify speed. 2.0 is twice as fast, etc.
-var SPEED_MODIFIER = 1.0;
-var SPEED = 1000 / SPEED_MODIFIER;
-var FRAME_INTERVAL = 20;
-
-var time;
-
-function init() {
+function init() {	
+	canvas = document.getElementById('screen');
+	context = canvas.getContext('2d');
+	baseRowCount = canvas.width / DIAMETER;
+	startTime = Date.now();
+	lastTime = startTime;
+	loadQueue();
+	update();
 	intervalId = setInterval(update, FRAME_INTERVAL);
 	window.requestAnimationFrame(draw);
-	time = Date.now();
 }
 
-var waterline = 0;
-var rate = 1000;
-var buffer = rate;
-var drops = [];
-var total = 0;
+var lastTime;
+var startTime;
+
+var baseRowCount;
+var rate = BASE_RATE;
+var buffer = 0;
+var queue = [];
+var used = 0;
+var row = 0;
+
+function loadQueue() {
+	let rowCount = baseRowCount - (row % 2);
+	for (let i = 0; i < rowCount; i++) {
+		queue.push({
+			x: RADIUS * (1 + i * 2 + (row % 2)),
+			y: -RADIUS,
+			v: 0,
+			row: row
+		});
+	}
+	shuffle(queue, used);
+}
 
 function update() {
-	rate = 1000 / (1 + total / 20);
-	let newTime = Date.now();
-	let elapsed = newTime - time;
-	time = newTime;
-	
+	let time = Date.now();
+	let elapsed = time - lastTime;
+	lastTime = time;
 	buffer += elapsed;
 	
 	let spawn = Math.floor(buffer / rate);
+	buffer %= rate;
+	used += spawn;
+	rate = Math.floor(BASE_RATE / (1 + SPEED * used / baseRowCount));
 	
-	if (spawn > 0) {
-		buffer %= rate;
-		for (let i = 0; i < spawn; i++) {
-			let r = 12 + Math.floor(Math.random() * 12);
-			drops.push({
-				x: Math.floor(Math.random() * canvas.width),
-				y: -r - Math.floor(Math.random() * 10),
-				r: r,
-				v: 0
-			})
-		}
+	if (spawn > queue.length - used) {
+		row++;
+		console.log(row + ' rows completed in ' + (time - startTime) / 1000 + ' sec');
+		loadQueue();
 	}
 
-	for (let i = drops.length - 1; i >= 0; i--) {
-		let drop = drops[i];
-		drop.v += elapsed * 1/128;
-		drop.y += drop.v;
-		if (drop.y - drop.r >= canvas.height - waterline) { 
-			drops.splice(i, 1);
-			total++;
-			console.log(total);
-			waterline += Math.PI * drop.r * drop.r / 1600;
-		}
+	for (let i = used - 1; i >= 0; i--) {
+		let drop = queue[i];
+		drop.v += elapsed * 1 / 128;
+		drop.y = Math.min(drop.y + drop.v, canvas.height - RADIUS - (drop.row * DIAMETER * SIN_60));
 	}
-	
 }
 
 function draw() {
-	canvas = document.getElementById('screen');
-	context = canvas.getContext('2d');
+	
 	context.clearRect(0, 0, canvas.width, canvas.height);
-	context.fillStyle = 'red';
-	for (let drop of drops) {
-		drawCircle(drop.x, drop.y, drop.r);
-	}
-	context.rect(0, canvas.height - waterline, canvas.width, waterline);
+	context.fillStyle = 'black';
+	
+	context.rect(0, 0, canvas.width, canvas.height);
 	context.fill();
+	context.fillStyle = 'white';
+	for (let drop of queue) {
+		drawCircle(drop.x, drop.y, RADIUS);
+	}
+
 	window.requestAnimationFrame(draw);
 }
 
@@ -76,6 +88,23 @@ function drawCircle(x, y, r) {
 	context.beginPath();
 	context.arc(x, y, r, 0, 2 * Math.PI);
 	context.fill();
+}
+
+
+function shuffle(array, start) {
+	for (let i = array.length - 1; i > start; i--) {
+		swap(array, i, randInt(start, i));
+	}
+}
+
+function randInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function swap(array, i, j) {
+	let temp = array[i];
+	array[i] = array[j];
+	array[j] = temp;
 }
 
 window.onload = init;
